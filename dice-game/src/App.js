cb9d3a6dcb3959b9fa6d51098335a489b98eb078
Fabian GREGOIRE <fabian.pro@protonmail.com>
2024-08-14 20:33:36 +0200

    fix notifications for doubles

--- dice-game/src/App.js
+++ dice-game/src/App.js
@@ -126,7 +126,7 @@ function App() {
             }, 3000);
         }
 
-        const handleChickenPlayerPenalties = ({ playerId, penalty }) => {
+        const handleChickenPlayerPenalties = ( playerId, penalty) => {
             const currentPenaltyId = nextPenaltyId; // Stocker l'ID actuel dans une variable locale
             nextPenaltyId++; // Incrémenter l'ID pour la prochaine notification
         
@@ -154,15 +154,13 @@ function App() {
                     spread: 200
                 });
                 if (doubleNumber === 1){
-                    handleChickenPlayerPenalties(99, `Et c'est la pénalité maximale pour ${playerName} !`);
-                    console.log(`Et c'est la pénalité maximale pour ${playerName} !`);
+                    handleChickenPlayerPenalties();
                 }else{
                     handleChickenPlayerPenalties(99, `${playerName} distribue 6 pénalités et peut ajouter une règle de son choix !`);
-                    console.log(`${playerName} distribue 6 pénalités et peut ajouter une règle de son choix !`);
                 }
             }else{
                 handleChickenPlayerPenalties(99, `${playerName} distribue ${doubleNumber} pénalités !`);
-                console.log(`${playerName} distribue ${doubleNumber} pénalités !`);
+                console.log(`handleDoubleEvent: ${playerName} distribue ${doubleNumber} pénalités !`);
             }
         }
 
